a3880e93c09d3551b227902a7b868f4ded00e36c
smasuda <shun@cryptact.com>
2024-06-05 09:08:28 +0900

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using QRCoder;
+
+namespace WifiQRCodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ssid = "MyWiFiNetwork";
+            string password = "123456789";
+            string encryption = "WPA"; // WPA, WEP, or nopass
+
+            string wifiString = $"WIFI:S:{ssid};T:{encryption};P:{password};;";
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(wifiString, QRCodeGenerator.ECCLevel.Q);
+                PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+                byte[] qrCodeBytes = qrCode.GetGraphic(20);
+
+                File.WriteAllBytes("wifi_qr_code.png", qrCodeBytes);
+                Console.WriteLine("QR code image saved as wifi_qr_code.png");
+            }
+        }
+    }
+}
